38094ead8a1e563ef191e93a07b9cc042e998fb9
Ganpati Kolhal <154478305+iam-ganpati@users.noreply.github.com>
2025-05-28 11:35:28 +0530

    Create jenkinsfile2.groovy

---
+++ jenkinsfile2.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+
+     parameters {
+        string(name: 'JENKINS_URL', defaultValue: 'http://13.217.37.46:8080/', description: 'Jenkins URL')
+        string(name: 'JENKINS_USER', defaultValue: ' ', description: 'Jenkins User')
+        password(name: 'JENKINS_TOKEN', defaultValue: '', description: 'your jenkins username')
+    }
+
+    environment {
+        SCRIPT_FILE = 'jenkins4.sh'
+    }
+
+    stages {
+        stage('Clone Script') {
+            steps {
+                git branch: 'main',
+                //credentialsId: 'your-git-credential-id',  // 👈 Git credentials
+                url: 'https://github.com/iam-ganpati/jenkins-list'
+            }
+        }
+
+        stage('Run Export Script') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'jenkins_api_creds', usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_TOKEN')]) {
+                    sh '''
+                        rm -f jenkins_jobs_*.csv
+                        chmod +x $SCRIPT_FILE
+                        ./$SCRIPT_FILE "$JENKINS_URL" "$JENKINS_USER" "$JENKINS_TOKEN"
+                    '''
+                }
+            }
+        }
+
+        stage('Archive Output') {
+            steps {
+                archiveArtifacts artifacts: '*.csv', fingerprint: true
+            }
+        }
+    }
+}
